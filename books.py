fa60a7dd9344ec2cfc15f689ca668787fad0b344
Charles <15911636+charlesGraham@users.noreply.github.com>
2025-05-20 00:00:05 -0700

    use api_router to preface rotues with 'api'

--- books.py
+++ books.py
@@ -1,16 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 
 app = FastAPI()
+api_router = APIRouter(prefix="/api")
 
 
 BOOKS = [
-    {"title": "Title One", "author": "Author One"},
-    {"title": "Title Two", "author": "Author Two"},
-    {"title": "Title Three", "author": "Author Three"},
-    {"title": "Title Four", "author": "Author Four"},
-    {"title": "Title Five", "author": "Author Five"},
+    {"title": "Title One", "author": "Author One", "category": "Science"},
+    {"title": "Title Two", "author": "Author Two", "category": "Science"},
+    {"title": "Title Three", "author": "Author Three", "category": "History"},
+    {"title": "Title Four", "author": "Author Four", "category": "Math"},
+    {"title": "Title Five", "author": "Author Five", "category": "Math"},
+    {"title": "Title Six", "author": "Author Two", "category": "Math"}
 ]
 
-@app.get("/api/books")
+@api_router.get("/books")
 async def get_books():
     return BOOKS
+
+    
+app.include_router(api_router)
